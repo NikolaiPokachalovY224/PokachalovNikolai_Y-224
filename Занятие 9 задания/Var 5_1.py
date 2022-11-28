c87e7dd06655054fee1092d9532d3a237160d63c
NikolaiPokachalovY224 <113941450+NikolaiPokachalovY224@users.noreply.github.com>
2022-11-28 19:56:15 +0300

    Add files via upload

---
+++ Занятие 9 задания/Var 5_1.py
@@ -0,0 +1,11 @@
+import random
+def matrsort():
+    N, M = 3, 3
+    a = [[random.randrange(1, 10, 1) for i in range(M)] for i in range(N)]
+    print(list([(1 if min(i)
+                 else 0)
+                if j == min(i)
+                else j
+                for j in i]
+               for i in a))
+matrsort()
