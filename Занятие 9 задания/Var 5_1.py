933338be4bf672a69db51eb4e44356b32ade47b5
NikolaiPokachalovY224 <113941450+NikolaiPokachalovY224@users.noreply.github.com>
2022-11-28 21:13:57 +0300

    Update Var 5_1.py

--- Занятие 9 задания/Var 5_1.py
+++ Занятие 9 задания/Var 5_1.py
@@ -1,11 +1,7 @@
 import random
 def matrsort():
     N, M = 3, 3
-    a = [[random.randrange(1, 10, 1) for i in range(M)] for i in range(N)]
-    print(list([(1 if min(i)
-                 else 0)
-                if j == min(i)
-                else j
-                for j in i]
-               for i in a))
+    a = [[random.randrange(20, 30, 1) for i in range(M)] for i in range(N)]
+    for i in a:
+        print(*sorted(i))
 matrsort()
