4d21670af4c1d9a6e5284bef5a2055febfe2e819
NikolaiPokachalovY224 <113941450+NikolaiPokachalovY224@users.noreply.github.com>
2023-05-08 23:51:24 +0300

    Update Main.java

--- Java/Z9/Main.java
+++ Java/Z9/Main.java
@@ -1,1 +1,13 @@
-
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первую строку:");
+        String firstString = scanner.nextLine();
+        System.out.println("Введите вторую строку:");
+        String secondString = scanner.nextLine();
+        Z8 z8 = new Z8();
+        boolean result = z8.isEnding(firstString, secondString);
+        System.out.println("Результат: " + result);
+    }
+}
