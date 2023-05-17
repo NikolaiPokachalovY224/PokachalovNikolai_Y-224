8887cc6288bdca11b1e43eff206bbf84b91bfbb9
NikolaiPokachalovY224 <113941450+NikolaiPokachalovY224@users.noreply.github.com>
2023-05-17 10:55:21 +0300

    Create Z17.java

---
+++ Java/KT3/Z17/Z17.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+class Z17 {
+    public static boolean checkBrackets(String expression) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < expression.length(); i++) {
+            char ch = expression.charAt(i);
+            if (ch == '(') {
+                stack.push(ch);
+            } else if (ch == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
