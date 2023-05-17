c6467aff391273780a1a2d41d9b2b3480d8c8b37
NikolaiPokachalovY224 <113941450+NikolaiPokachalovY224@users.noreply.github.com>
2023-05-17 17:33:46 +0300

    Create Main.java

---
+++ Java/KT3/Z24/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите начальное число M: ");
+        int M = scanner.nextInt();
+        System.out.print("Введите конечное число N: ");
+        int N = scanner.nextInt();
+        System.out.print("Введите количество делителей: ");
+        int numDivisors = scanner.nextInt();
+        int[] divisors = new int[numDivisors];
+        System.out.println("Введите делители:");
+        for (int i = 0; i < numDivisors; i++) {
+            divisors[i] = scanner.nextInt();
+        }
+        int sum = Z24.Sum(M, N, divisors);
+        System.out.println("Сумма чисел, делящихся на один из делителей без остатка: " + sum);
+        scanner.close();
+    }
+}
