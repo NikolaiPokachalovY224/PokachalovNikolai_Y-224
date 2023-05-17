bc95e33a98d2608d0d6f4500bb16848240631ac3
NikolaiPokachalovY224 <113941450+NikolaiPokachalovY224@users.noreply.github.com>
2023-05-17 11:06:26 +0300

    Create Main.java

---
+++ Java/KT3/Z18/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите размер массива: ");
+        int size = scanner.nextInt();
+        int[] array = new int[size];
+        System.out.println("Введите элементы массива:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+        scanner.close();
+        Z18.findMaxAndSplit(array);
+    }
+}
